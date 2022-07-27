ac91878e70b54147096ff47d7d46fe1aba8b9830
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-27 10:51:36 +0430

    Add files via upload

---
+++ 3_while_for/Q1.c
@@ -0,0 +1,23 @@
+/* 
+
+https://quera.org/problemset/8838/
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    char str[100];
+    
+    scanf("%d %s", &n, &str);
+    
+    while(n-- > 0){
+        
+        printf("copy of ");
+    }
+    
+    printf("%s", str);
+}
+
---
+++ 3_while_for/Q2.c
@@ -0,0 +1,23 @@
+/* https://leetcode.com/problems/goat-latin/
+
+
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    char str[100];
+    
+    scanf("%d %s", &n, &str);
+    
+    while(n-- > 0){
+        
+        printf("copy of ");
+    }
+    
+    printf("%s", str);
+}
+
