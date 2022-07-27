a0362b869ffee2ad3721eeda9d142022fab6d808
Sajad Salehi <102877977+Sajad-Salehi@users.noreply.github.com>
2022-07-27 11:53:56 +0430

    Add files via upload

---
+++ 2_if_else/Q7.c
@@ -0,0 +1,23 @@
+/* 
+
+https://quera.org/problemset/10162
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+
+    if(n % 2 == 0)
+    	printf("Bala Barare");
+    
+    else
+    	printf("Payin Barare");
+    
+
+}
+
